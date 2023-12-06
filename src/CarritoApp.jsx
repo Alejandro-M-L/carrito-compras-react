6ddf1da2e83b7a1acd9005524940eb5106cca37e
alejandro <a86alejandro@gmail.com>
2023-12-05 20:18:16 -0600

    Carrito de compras

---
+++ src/CarritoApp.jsx
@@ -0,0 +1,26 @@
+import { Navigate, Routes, Route } from "react-router-dom";
+import { NavBar } from "./components/NavBar";
+import { CarritoPage } from "./pages/CarritoPage";
+import { ComprasPage } from "./pages/ComprasPage";
+import { ProductosProvider } from "./context/ProductosProvider";
+import { CarritoProvider } from "./context/CarritoProvider";
+export const CarritoApp = () => {
+  return (
+    <ProductosProvider>
+      <CarritoProvider>
+        <NavBar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<ComprasPage></ComprasPage>}></Route>
+            <Route path="/carrito" element={<CarritoPage></CarritoPage>}></Route>
+            <Route path="/*" element={<Navigate to="/" />}></Route>
+          </Routes>
+        </div>
+      </CarritoProvider>
+    </ProductosProvider>
+  );
+};
+
+//https://mui.com/material-ui/getting-started/installation/
+//npm install @mui/material @emotion/react @emotion/styled
+//npm install @mui/icons-material
